c28f0ae0118a64541dd70303ffb8812f60b9d38b
Javier González Soldado <95412468+botij0@users.noreply.github.com>
2026-06-27 19:12:23 +0200

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = {}
+
+        for i in range(numCourses):
+            adj[i] = []
+        
+        print(adj)
+
+        for src, dst in prerequisites:
+            adj[src].append(dst)
+        
+        visit = set()
+        path = set()
+
+        for i in range(numCourses):
+            if  not self.dfs(i, adj, visit, path):
+                return False
+        
+        return True
+
+    
+    def dfs(self, src: int, adj: dict, visit:set, path:set):
+        if src in path:
+            return False
+
+        if src in visit:
+            return True
+        
+        visit.add(src)
+        path.add(src)
+
+        for neighbor in adj[src]:
+            if not self.dfs(neighbor, adj, visit, path):
+                return False
+        
+        path.remove(src)
+        return True
+
+        # topSort.append(src)
