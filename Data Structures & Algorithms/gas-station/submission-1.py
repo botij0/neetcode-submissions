caf34c5dae1c60ff4020bc18d409f0a4b13f9ae6
Javier González Soldado <95412468+botij0@users.noreply.github.com>
2026-07-27 13:27:03 +0200

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        startOptions = []
+        m = 0
+        L = len(gas)
+        for i in range(L):
+            aux = gas[i] - cost[i]
+            if  aux >= 0:
+                startOptions.append(i)
+                m = aux
+        
+
+        for start in startOptions:
+            visited = set()
+            currentGas = gas[start]
+            r = 0
+
+            while start not in visited:
+                visited.add(start)
+
+                currentGas = currentGas - cost[start]
+                if currentGas < 0:
+                    r = -1
+                    break
+                
+                start = (start + 1) % L
+                currentGas += gas[start]
+            
+            if r == 0:
+                return start
+            
+
+        return -1
