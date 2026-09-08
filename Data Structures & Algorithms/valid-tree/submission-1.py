96f0b8fd0fedd9edce340e1e5aa4665ff67d691d
Javier González Soldado <95412468+botij0@users.noreply.github.com>
2026-09-08 12:48:12 +0200

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,74 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = {}
+        self.rank = {}
+        self.comps = n
+    
+        for i in range(n+1):
+            self.parent[i] = i
+            self.rank[i] = 0
+    
+    def find(self, n):
+        if n != self.parent[n]:
+            self.parent[n] = self.find(self.parent[n])
+        
+        return self.parent[n]
+    
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+
+        if p1 == p2:
+            return False
+        
+        self.comps -= 1
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+        elif self.rank[p1] < self.rank[p2]:
+            self.parent[p1] = p2
+        else:
+            self.parent[p1] = p2
+            self.rank[p2] += 1
+        
+        return True
+
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        #return self.unionFindSol(n, edges)\
+        adj = {}
+        for i in range(n):
+            adj[i] = []
+        
+        for src, dst in edges:
+            adj[src].append(dst)
+            adj[dst].append(src)
+        
+        visited = set()
+        hasNotCycle = self.dfs(adj, visited, 0, -1)
+
+        return hasNotCycle and len(visited) == n
+
+    def dfs(self, adj: dict, visited: set, node: int, parent: int):
+        if node in visited:
+            return False
+        
+        visited.add(node)
+        for neigh in adj[node]:
+            if neigh == parent:
+                continue
+            if not self.dfs(adj, visited, neigh, node):
+                return False
+
+        return True
+
+
+
+    def unionFindSol(self, n: int, edges: List[List[int]]) -> bool:
+        dsu = UnionFind(n)
+        for u, v in edges:
+            if not dsu.union(u,v):
+                return False
+        
+        return dsu.comps == 1
+
+        
